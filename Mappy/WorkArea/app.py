08de5e46df82a74e7065ad1603ac083127a88824
haeun161 <haeun161@naver.com>
2024-05-31 13:13:48 +0900

    fix: 헤더 추가, html 분리

--- Mappy/WorkArea/app.py
+++ Mappy/WorkArea/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/traffic')
+def traffic():
+    return render_template('traffic.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
