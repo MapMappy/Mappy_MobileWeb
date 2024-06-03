3d6bc9ecdbb0d070fe3154f65a2a76795c1b9dc2
haeun161 <haeun161@naver.com>
2024-06-03 11:18:48 +0900

    fix: Google Map 성공

--- Mappy/WorkArea/app.py
+++ Mappy/WorkArea/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+# .env 파일 로드
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -13,6 +18,11 @@ def schedule():
 @app.route('/search')
 def search():
     return render_template('search.html')
+
+@app.route('/map')
+def map():
+    api_key = os.getenv('GOOGLE_MAPS_API_KEY')
+    return render_template('mapApi/ui.html', api_key=api_key)
 
 @app.route('/traffic')
 def traffic():
